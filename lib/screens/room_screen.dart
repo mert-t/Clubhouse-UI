488bea79d96f7be5e8c46e155c78b40cee67760e
mert-t <temiz.mertt@gmail.com>
2021-09-09 11:05:00 +0300

    room screen and appbar added

---
+++ lib/screens/room_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clubhouse_ui/widgets/user_profile_image.dart';
+
+import '../data.dart';
+
+class RoomScreen extends StatelessWidget {
+  final Room room;
+  const RoomScreen({
+    Key? key,
+    required this.room,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 120.0,
+        elevation: 0,
+        leading: TextButton.icon(
+          style: TextButton.styleFrom(primary: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(CupertinoIcons.chevron_down),
+          label: const Text("All rooms"),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.doc, size: 28.0,color: Colors.black,),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 10.0, 20.0, 10.0),
+              child: UserProfileImage(
+                imageUrl: currentUser.imageUrl,
+                size: 36.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
